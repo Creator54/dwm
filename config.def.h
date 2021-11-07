6bf7f64e4ec007f610f57b533448c35ee519de13
creator54 <hi.creator54@gmail.com>
2021-11-08 00:50:31 +0530

    disable switching workspace with ctrl+tab

--- config.def.h
+++ config.def.h
@@ -166,7 +166,7 @@ static Key keys[] = {
     { MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
     { MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
     //{ MODKEY,                       XK_Return, zoom,           {0} },
-    { MODKEY,                       XK_Tab,    view,           {0} },
+    //{ MODKEY,                       XK_Tab,    view,           {0} },
 
     // overall gaps
     { MODKEY|ControlMask,           XK_i,      incrgaps,       {.i = +1 } },
